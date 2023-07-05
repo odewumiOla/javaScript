332da08c9f925024b23b3955c246522b18181a26
Thewealth <odewumiolajide@gmail.com>
2023-07-05 13:25:01 -0700

    The first assignment

---
+++ trial.js
@@ -0,0 +1,22 @@
+
+//  Question One
+let name = "John Smith";
+let age = 20;
+// using a Boolean value to check if the patient is new or an existing patient, (this is set true, that means the patient is new )
+let isNewPatient = true;
+
+// Question two
+/*
+const firstName = prompt("Enter your First Name:");
+const lastName = prompt("Enter your Last Name:");
+ 
+console.log(`${firstName} ${lastName}`)*/
+
+// Question Three
+
+/* Since the input value is requested to be integer, so the users input is converted with parseInt buit-in funtion. */ 
+const num_one = parseInt(prompt("Enter the first number: "));
+const num_two = parseInt(prompt("Enter the second number: "));
+const sum_num = num_one + num_two;
+
+// console.log(`The sum is:, ${sum_num}`);
